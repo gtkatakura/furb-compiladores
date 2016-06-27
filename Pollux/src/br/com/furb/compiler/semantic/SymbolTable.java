a4a8872c7b6a46771f8040bad9f49bcb437e195b
gtkatakura <gt.katakura@gmail.com>
2016-06-27 01:17:54 -0300

    add(SymbolTable): adicionado types

--- Pollux/src/br/com/furb/compiler/semantic/SymbolTable.java
+++ Pollux/src/br/com/furb/compiler/semantic/SymbolTable.java
@@ -1,13 +1,24 @@
 package br.com.furb.compiler.semantic;
 
+import java.util.Stack;
+
 public class SymbolTable {
 	private Identifier identifier;
+	private Stack<String> types;
 	
-	public void setIdentifier(String identificador) {
-		this.identifier = new Identifier(identificador);
+	public SymbolTable() {
+		this.types = new Stack<String>();
+	}
+	
+	public void setIdentifier(Identifier identifier) {
+		this.identifier = identifier;
 	}
 	
 	public Identifier getIdentifier() {
 		return this.identifier;
+	}
+	
+	public Stack<String> getTypes() {
+		return this.types;
 	}
 }
